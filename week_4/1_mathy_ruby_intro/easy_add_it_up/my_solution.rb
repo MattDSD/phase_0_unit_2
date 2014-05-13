4385d20289f640e45cfa422628ddf9cf939a3e87
Matthew Darin <mgd2233@gmail.com>
2014-05-12 20:49:13 -0700

    updated psuedo

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -8,6 +8,15 @@
 
 # 1. Pseudocode
 
+## Pseudocode
+## method called total that takes an array of numbers and returns the sum of all the numbers
+## method that joins an array into one string 
+## ---------
+##  define a method called total that takes in a parameter of an array
+##  create a variable called sum that equals zero 
+## loop through all the numbers in the array and add each one to sum
+## return sum
+
 # What is the input?
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
