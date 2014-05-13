8c9a5489e39f6cb0a509e53b307c2d3ab412dff0
Matthew Darin <mgd2233@gmail.com>
2014-05-12 23:05:53 -0700

    Completed mathy assignment

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -17,18 +17,53 @@
 ## loop through all the numbers in the array and add each one to sum
 ## return sum
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+
+# What is the input? integers in an array 
+# What is the output? (i.e. What should the code return?) the sum (total) of the array elements 
+# What are the steps needed to solve the problem? 
+# Define a variable 
+# iterate over the array 
+# return sum 
+
 
 
 # 2. Initial Solution
+
+def total(array)
+  sum = 0
+  for i in array 
+    sum += i
+  end
+  sum
+end
+
+## Define a method called sentence_maker that takes in an array 
+## returns each element joined into one sentence
+## the first letter of the sentence should be capitalized and it should end with a period. 
+
+## capitalize the array, then join each element, then add a period. 
+
+
+def sentence_maker(array)
+  array.first.capitalize!
+  array.join(" ") + "."
+end
 
 
 
 
 # 3. Refactored Solution
 
+## We did not need to refactor our solution.  The code was 
+## clean from the beginning. 
+
 
 
 # 4. Reflection 
+# I learned a lot in this section.  We were able to work through this code
+# fairly quickly.  The aha moment on this exercise came when I FINALLY 
+# learned how to use git commands within the terminal.  I feel like 90% of my stress 
+# will be alleviated now that I know basic git commands. 
+
+# Code-wise, everything worked the first time around.  Arik and I were communicating well 
+# and talking through things in a very efficient manner. 
--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -8,18 +8,64 @@
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+##Pseudocode Medium Problem
+## Create method called "Get Grade" that takes an array of test scores 
+## each score in the array should be between 0 and 100, where 100 is the max score
+## We are computing the average score and returning it as a letter grade 
+## We will loop through each element in the array and if any elements in the array are negative or 
+## above 100, we will return false.  Otherwise, we will create a variable called sum, which will 
+## take the total of all of the numbers added up. Then we will create a variable called average, which
+## is equal to the total divided by the length of the array. If the average is between 90-100, return "A"
+## if average is between 80-90 return "B", if between 70-80 return "C".  If between 60-70, return "D" 
+## and everything below will be "F". 
+
+# What is the input? the number grades in an array 
+# What is the output? (i.e. What should the code return?) a letter grade 
+# What are the steps needed to solve the problem? 
+=begin
+we created code that computes an average score in an array and returns a letter grade. 
+we used switch cases to define the letter grade parameters. 
+
+
+
+=end 
 
 
 # 2. Initial Solution
 
+def get_grade(array)
+	sum = 0
+    array.each do |x|
+    	sum += x
+    end
+    average = sum / array.length
+    case average 
+    when 90..100
+      "A"
+    when 80..89
+      "B"
+    when 70..79
+      "C"
+    when 60..69
+      "D"
+    else
+      "F"
+  end
+ end 
 
 
 
 # 3. Refactored Solution
-
-
+# No refactored code 
 
 # 4. Reflection 
+=begin 
+Same as the last exercise, I paired with Arik to complete this exercise.
+We used switch cases, which was new to me, to complete this code.  It was fun 
+to go outside of our comfort zone and use case statements. 
+The major aha moment I had was when I finally learned how to use git commands to commit my work.
+
+At this moment, I feel a little rusty at Ruby, but I am feeling more confident with each exercise.
+
+
+=end 
