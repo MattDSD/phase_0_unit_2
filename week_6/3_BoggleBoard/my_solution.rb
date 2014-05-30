733db0671d81cbac4bd3f5529d1c2a02cc3c47d3
Matthew Darin <mgd2233@gmail.com>
2014-05-29 19:10:17 -0700

    complete

--- week_6/3_BoggleBoard/my_solution.rb
+++ week_6/3_BoggleBoard/my_solution.rb
@@ -1,18 +1,40 @@
 # U2.W6: Create a BoggleBoard Class
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself, with: Arik Gadye ].
 
 
 # 2. Pseudocode
 
 
 
+=begin
+Create word method 
+def create_word(boogle_board, *coords)
+  coords.map { |coord| boggle_board[coord.first][coord.last]}.join("")
+end
+
+puts create_word(boggle_board, [2,1], [1,1], [1,2], [0,3])  #=> returns "code"  
+puts create_word(boggle_board, [0,1], [0,2], [1,2])  #=> returns "rad"
+
+# Refactored Solution
+
+
+
+# DRIVER TESTS GO BELOW THIS LINE
+p create_word(boggle_board, [0,0], [1,1], [0,2], [1,3])  == "boat"
+
+=end 
+
 # 3. Initial Solution
 class BoggleBoard
- 
-  #your code here
- 
+	attr_accessor :board
+ def initialize(board)
+ 	@board = board
+ end
+ def create_word(*coords)
+ 	coords.map { |coord| @board[coord.first][coord.last]}.join("")
+ end
 end
  
  
@@ -22,6 +44,12 @@ dice_grid = [["b", "r", "a", "e"],
              ["t", "a", "k", "e"]]
  
 boggle_board = BoggleBoard.new(dice_grid)
+
+p boggle_board.create_word([1,2], [1,1], [2,1], [3,2]) == 'dock'
+p boggle_board.get_row(1) == 'iodt'
+p boggle_board.get_column(1) == 'roca'
+
+
  
 
 
@@ -35,6 +63,15 @@ boggle_board = BoggleBoard.new(dice_grid)
 # 1. DRIVER TESTS GO BELOW THIS LINE
 # create driver test code to retrieve a value at a coordinate here:
 # implement tests for each of the methods here:
-
+p boggle_board.create_word([1,2], [1,1], [2,1], [3,2]) == 'dock'
+p boggle_board.get_row(1) == 'iodt'
+p boggle_board.get_column(1) == 'roca'
 
 # 5. Reflection 
+=begin
+This was a fun exercise.  We were able to complete it fairly quickly.  This lesson actually 
+taught me more about how to write my own test code more effectively.  Everything worked out fairly 
+well.  It took me a moment to think about what needed to be done.  After we figured out the first 
+test, the rest flowed pretty quickly. 
+
+=end 
